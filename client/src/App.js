90a98cdf938da23cfbaea3345e25999a126fa15b
Ajay Tom George <ajaytomgeorge@gmail.com>
2022-03-26 22:08:19 +0000

    Add Login and Signup Pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
+
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <div className="App">
+            <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="sign-up" element={<SignUp/>} />
+            <Route index element={<HomePage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 
 import Header from "../Components/Header";
@@ -6,16 +7,23 @@ import Tags from "../Components/Tags";
 import Trending from "../Components/Trending";
 import JoinNow from "../Components/JoinNow";
 
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+
 export default function Homepage() {
+  const [storyMode, setstoryMode] = useState(false);
+
   return (
-    <div className="App">
-      <Container className="main_container d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom shadow">
-        <Header />
-        <Introduction />
-        <Tags />
-        <Trending />
-        <JoinNow />
-      </Container>
-    </div>
+    <Container className="main_container d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom shadow">
+      <Header />
+      {storyMode && (
+        <React.Fragment>
+          <Introduction />
+          <Tags />
+          <Trending />
+          <JoinNow />
+        </React.Fragment>
+      )}
+    </Container>
   );
 }
