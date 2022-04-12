fb3a35943d4e879e8f27706881aa87223c95417c
Ajay Tom George <ajaytomgeorge@gmail.com>
2022-04-12 23:05:49 +0100

    Add test cases

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,33 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from "react";
+import Enzyme from "enzyme";
+import Header from "./Components/Header";
+import Intro from "./Components/Intro";
+import JoinNow from "./Components/JoinNow";
+import StoryHeader from "./Components/StoryHeader";
+import Tags from "./Components/Tags";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+const { shallow } = Enzyme; //whatever you want to use here
 
-test( 'Fake Test', () => {
-  expect(true).toBeTruthy();
-})
+
+
+test("component exists", () => {
+  expect(Header).toBeDefined();
+});
+
+test("component exists", () => {
+  expect(Intro).toBeDefined();
+});
+
+
+test("component exists", () => {
+  expect(JoinNow).toBeDefined();
+});
+
+test("component exists", () => {
+  expect(StoryHeader).toBeDefined();
+});
+
+test("component exists", () => {
+  expect(Tags).toBeDefined();
+});
+
